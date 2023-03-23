74c8c5c5eb4aaf8b946a13837484e6ff3f4bf7ff
DaniMerchOut <70753263+DaniMerchOut@users.noreply.github.com>
2023-03-23 15:49:34 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Test from "./components/card/index.jsx";
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import Button from 'react-bootstrap/Button';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>TUTORIAL ASSIGNMENT</h1>
+      {/* <Button variant="primary">Primary</Button>{' '} */}
+      <Test ></Test>
+      </div>
+  );
+}
+
+export default App;
